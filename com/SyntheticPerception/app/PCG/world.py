6bf2ee89de74f2a8e1d6050f826ad96e76a64e86
JonER <jonathanembleyriches@outlook.com>
2023-04-24 15:30:36 +0100

    JSON reader for environment started.

--- com/SyntheticPerception/app/PCG/world.py
+++ com/SyntheticPerception/app/PCG/world.py
@@ -1,3 +1,9 @@
+import json
+
+class Environment:
+    def __init__(self):
+        self.regions = []
+
 class Region:
     def __init__(self):
 
@@ -16,3 +22,33 @@ class XObject:
         self.position = 0
         self.orientation = 0
         self.poisson_size = 0
+
+class EnvParamReader:
+
+    def __init__(self):
+
+        self._file_path = ""
+
+
+    def JSON_to_obj(self):
+        pass
+
+
+"""
+
+{
+  "Environment": {
+    "Regions": {
+      "Zones": {
+        "XObject": {
+          "name": "name",
+          "position": "pos",
+          "orientation": "ori",
+          "poisson_size": "size"
+        }
+      }
+    }
+  }
+}
+
+"""
