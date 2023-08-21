c0d9c331b46e506b8bede491a154a3d7314fbbe7
Jonathan <jonathanembleyriches@outlook.com>
2023-08-21 14:43:05 +0100

    Refixed imports

--- com/SyntheticPerception/app/core/environment.py
+++ com/SyntheticPerception/app/core/environment.py
@@ -1,5 +1,23 @@
 from omni.isaac.kit import SimulationApp
 
+from pxr import Usd, Gf, UsdGeom
+from omni.isaac.core.utils.stage import (
+    add_reference_to_stage,
+    is_stage_loading,
+    update_stage_async,
+    update_stage,
+)
+import omni
+from omni.isaac.core import World
+from omni.isaac.quadruped.robots import Anymal
+from omni.isaac.core.utils.prims import define_prim, get_prim_at_path
+from omni.isaac.core.utils.nucleus import get_assets_root_path
+from pxr import Gf, UsdGeom
+import time
+import omni.appwindow  # Contains handle to keyboard
+import numpy as np
+import carb
+from omni.isaac.core.utils.extensions import enable_extension
 
 class Environment:
     def __init__(self) -> None:
@@ -26,24 +44,6 @@ class IsaacHandler:
     def __init__(self, physics_dt, render_dt) -> None:
         self.simulation_app = SimulationApp({'headless': False})
 
-        from pxr import Usd, Gf, UsdGeom
-        from omni.isaac.core.utils.stage import (
-            add_reference_to_stage,
-            is_stage_loading,
-            update_stage_async,
-            update_stage,
-        )
-        import omni
-        from omni.isaac.core import World
-        from omni.isaac.quadruped.robots import Anymal
-        from omni.isaac.core.utils.prims import define_prim, get_prim_at_path
-        from omni.isaac.core.utils.nucleus import get_assets_root_path
-        from pxr import Gf, UsdGeom
-        import time
-        import omni.appwindow  # Contains handle to keyboard
-        import numpy as np
-        import carb
-        from omni.isaac.core.utils.extensions import enable_extension
 
         enable_extension('omni.kit.asset_converter')
         self.simulation_app.update()
